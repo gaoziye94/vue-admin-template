6ed7c9f2d1b847a3919bc4ae66b36b6216bb1c1b
gml <18368854213@163.com>
2022-04-16 16:36:51 +0800

    Initial commit

---
+++ src/views/system/dict.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="app-container">
+    <el-button @click="insertDict">新增</el-button>
+    <el-button @click="deleteDict">删除</el-button>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column
+        type="selection"
+        width="55">
+      </el-table-column>
+      <el-table-column align="center" label="ID" >
+        <template slot-scope="scope">
+          {{ scope.$index }}
+        </template>
+      </el-table-column>
+      <el-table-column label="字典名称"  align="center" >
+        <template slot-scope="scope">
+          {{ scope.row.dictName }}
+        </template>
+      </el-table-column>
+      <el-table-column label="字典编码"  align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.dictCode }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="备注"  align="center" >
+        <template slot-scope="scope">
+          {{ scope.row.remark }}
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="创建人" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.createdBy }}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="创建时间">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.createdTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="修改人"  align="center">
+        <template slot-scope="scope">
+          {{ scope.row.updatedBy }}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="修改时间"  >
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.updatedTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作"  align="center">
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              更多<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>编辑字典项</el-dropdown-item>
+              <el-dropdown-item>刷新字典项</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { getListPage } from '@/api/dict'
+
+export default {
+  name: 'Dict',
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      multipleSelection: []
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    // 编辑字典项
+    editItem(val) {
+      this.$router.push({
+        path: '/system/dictItem',
+        query: {
+          dictCode: val.msg.dictCode,
+          project: this.$store.state.user.project
+        }
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    insertDict() {
+
+    },
+    deleteDict() {
+
+    },
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    },
+    fetchData() {
+      this.listLoading = true
+      getListPage().then(response => {
+        this.list = response.data.records
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
+<style scoped="scoped">
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF !important;
+  font-size: 12px !important;
+  margin-left: 10px !important;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+</style>
---
+++ src/views/system/dict1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    哈哈哈哈哈11
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dict1'
+}
+</script>
+
+<style scoped>
+
+</style>
